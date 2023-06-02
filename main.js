1ca4146371a49065ca7e30aa95786bd50dfca94a
fneel <70952301+fneel@users.noreply.github.com>
2023-06-02 18:02:02 +0200

    html base and js fetch

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-
+function logWeatherTest() {
+    fetch("https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=39.099724&lon=-94.578331&dt=1643803200&appid=afa7998302266a1d34e7bcfa131689d0")
+    .then((response) => {
+        return response.json()
+    })
+    .then()
+}
