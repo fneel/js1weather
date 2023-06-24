ef61750e0f4df01bc5888a6bf9f95de32069f816
fneel <70952301+fneel@users.noreply.github.com>
2023-06-24 20:51:52 +0200

    färdigställt

--- main.js
+++ main.js
@@ -5,46 +5,51 @@ const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
 
-async function checkWeather(city){
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-    if(response.status == 404){
+  if (response.status == 404) {
     document.querySelector(".error").style.display = "block";
     document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
 
-    }else{
-      var data = await response.json();
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + " °C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " m/s";
+    document.querySelector(".pressure").innerHTML = data.main.pressure + " hPa";
 
-      document.querySelector(".city").innerHTML = data.name;
-      document.querySelector(".temp").innerHTML =
-        Math.round(data.main.temp) + " °C";
-      document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
-      document.querySelector(".wind").innerHTML = data.wind.speed + " m/s";
-      document.querySelector(".pressure").innerHTML =
-        data.main.pressure + " hPa";
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Snow") {
+      weatherIcon.src = "images/snow.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "images/mist.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "images/drizzle.png";
+    }
 
-      if (data.weather[0].main == "Clouds") {
-        weatherIcon.src = "images/clouds.png";
-      } else if (data.weather[0].main == "Clear") {
-        weatherIcon.src = "images/clear.png";
-      } else if (data.weather[0].main == "Rain") {
-        weatherIcon.src = "images/rain.png";
-      } else if (data.weather[0].main == "Snow") {
-        weatherIcon.src = "images/snow.png";
-      } else if (data.weather[0].main == "Mist") {
-        weatherIcon.src = "images/mist.png";
-      } else if (data.weather[0].main == "Drizzle") {
-        weatherIcon.src = "images/drizzle.png";
-      }
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
 
-      document.querySelector(".weather").style.display = "block";
-      document.querySelector(".error").style.display = "none";
+    console.log(data);
+  }
+}
 
-      //det här FUNGERAR iallafall
-      // const d = new Date();
-      // let text = d.toDateString();
-      // document.getElementsByClassName("time").innerHTML = text;
-      // document.querySelector(".time").innerHTML = d;
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
+
+setInterval(() => {
+  checkWeather(searchBox.value);
+}, 1 * 60 * 1000);
+
 
       // Hämta element från DOM för att visa tid och datum
       const city = document.getElementById("city");
@@ -69,10 +74,12 @@ async function checkWeather(city){
       // Uppdatera tid och datum varje sekund
       setInterval(updateDateTime, 1000);
 
-      console.log(data);
-    }
-}
 
+      //det här FUNGERAR iallafall
+      // const d = new Date();
+      // let text = d.toDateString();
+      // document.getElementsByClassName("time").innerHTML = text;
+      // document.querySelector(".time").innerHTML = d;
 
       // Här antar vi att du redan har hämtat stadsinformationen från OpenWeatherMap API och har en variabel som heter 'city' som innehåller stadens namn.
 
@@ -82,12 +89,6 @@ async function checkWeather(city){
       // };
 
 
-
-searchBtn.addEventListener("click", ()=>{
-    checkWeather(searchBox.value);
-
-
-})
 
 
 
